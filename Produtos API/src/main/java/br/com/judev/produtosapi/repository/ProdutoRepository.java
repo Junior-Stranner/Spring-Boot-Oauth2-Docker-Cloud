c275fd6bf299bde5c1fefff3461507f86131e606
Junior-Stranner <JuniorStranner@gmail.com>
2026-02-10 09:39:24 -0300

    implementing the basic structure with complete content

---
+++ Produtos API/src/main/java/br/com/judev/produtosapi/repository/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package br.com.judev.produtosapi.repository;
+
+import br.com.judev.produtosapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
+}
