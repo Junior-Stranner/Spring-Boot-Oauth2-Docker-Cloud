7cfe9689ce8ee70b0de435aa1c3b444ebb7abd97
Junior-Stranner <JuniorStranner@gmail.com>
2026-02-13 17:22:44 -0300

    add transacional and demonstrate how it works

---
+++ libraryapi/src/test/java/br/com/judev/libraryapi/repository/TransacoesTest.java
@@ -0,0 +1,26 @@
+package br.com.judev.libraryapi.repository;
+
+import br.com.judev.libraryapi.service.TransacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TransacoesTest {
+
+    @Autowired
+    TransacaoService transacaoService;
+
+    /**
+     * Commit -> confirmar as alterações
+     * Rollback -> desfazer as alterações
+     */
+    @Test
+    void transacaoSimples(){
+        transacaoService.executar();
+    }
+
+    @Test
+    void transacaoEstadoManaged(){
+        transacaoService.atualizacaoSemAtualizar();
+    }
+}
