205d4bc4eb1a5c73b49494997a29cf2490189939
Junior-Stranner <JuniorStranner@gmail.com>
2026-02-13 11:43:14 -0300

    add all entitys that gonna be used on this leacture

---
+++ libraryapi/src/main/java/br/com/judev/libraryapi/model/Autor.java
@@ -0,0 +1,44 @@
+package br.com.judev.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "autor", schema = "public")
+@Getter@Setter
+@ToString(exclude =  {"livros"})
+
+public class Autor {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Livro> livros;
+
+    @Deprecated
+    public Autor() {
+        //Para uso do Framework
+    }
+
+    public Autor(String nome, LocalDate dataNascimento, String nacionalidade) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.nacionalidade = nacionalidade;
+    }
+}
