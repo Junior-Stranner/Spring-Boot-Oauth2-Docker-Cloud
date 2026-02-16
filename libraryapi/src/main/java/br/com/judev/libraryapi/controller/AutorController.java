dca5c2b5b4c17f1e85c40713b2a6f4f0642e6c1e
Junior-Stranner <JuniorStranner@gmail.com>
2026-02-16 19:38:34 -0300

    complete CRUD

--- libraryapi/src/main/java/br/com/judev/libraryapi/controller/AutorController.java
+++ libraryapi/src/main/java/br/com/judev/libraryapi/controller/AutorController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/autores")
@@ -34,6 +36,25 @@ public class AutorController {
         return ResponseEntity.created(location).build();
     }
 
+    @PutMapping("{id}")
+    public ResponseEntity<Void> atualizar(@PathVariable("id") String id, @RequestBody @Valid AutorDTO dto) {
+
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = autorService.obterPorId(idAutor);
+
+        if (autorOptional.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        var autor = autorOptional.get();
+        autor.setNome(dto.nome());
+        autor.setNacionalidade(dto.nacionalidade());
+        autor.setDataNascimento(dto.dataNascimento());
+
+        autorService.atualizar(autor);
+
+        return ResponseEntity.noContent().build();
+    }
+
     @GetMapping("{id}")
     public ResponseEntity<AutorDTO> obterDetalhes(@PathVariable("id") String id) {
         var idAutor = UUID.fromString(id);
@@ -47,6 +68,21 @@ public class AutorController {
             return ResponseEntity.ok(dto);
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> pesquisar(
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "nacionalidade", required = false) String nacionalidade) {
+        List<Autor> resultado = autorService.pesquisaByExample(nome, nacionalidade);
+        List<AutorDTO> lista = resultado
+                .stream()
+                .map(autor -> new AutorDTO(
+                        autor.getId(),
+                        autor.getNome(),
+                        autor.getDataNascimento(), autor.getNacionalidade())
+                ).collect(Collectors.toList());
+        return ResponseEntity.ok(lista);
     }
 
     @DeleteMapping("{id}")
--- libraryapi/src/main/java/br/com/judev/libraryapi/service/AutorService.java
+++ libraryapi/src/main/java/br/com/judev/libraryapi/service/AutorService.java
@@ -3,8 +3,11 @@ package br.com.judev.libraryapi.service;
 import br.com.judev.libraryapi.model.Autor;
 import br.com.judev.libraryapi.repository.AutorRepository;
 import br.com.judev.libraryapi.repository.LivroRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -30,11 +33,53 @@ public class AutorService {
                 .orElseThrow(() -> new RuntimeException("Autor não encontrado."));
 
         boolean temLivros = livroRepository.existsByAutorId(id);
-
         if (temLivros) {
             throw new RuntimeException("Não é permitido excluir um Autor que possui livros cadastrados!");
         }
 
         autorRepository.delete(autor);
+    }
+
+    public List<Autor> pesquisa(String nome, String nacionalidade){
+        if(nome != null && nacionalidade != null){
+            return autorRepository.findByNomeAndNacionalidade(nome, nacionalidade);
+        }
+
+        if(nome != null){
+            return autorRepository.findByNome(nome);
+        }
+
+        if(nacionalidade != null){
+            return autorRepository.findByNacionalidade(nacionalidade);
+        }
+
+        return autorRepository.findAll();
+    }
+
+    public void atualizar(Autor autor){
+        if(autor.getId() == null){
+            throw new IllegalArgumentException("Para atualizar, é necessário que o autor já esteja salvo na base.");
+        }
+    //    validator.validar(autor);
+        autorRepository.save(autor);
+    }
+
+    public List<Autor> pesquisaByExample(String nome, String nacionalidade){
+        var autor = new Autor();
+        autor.setNome(nome);
+        autor.setNacionalidade(nacionalidade);
+
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnorePaths("id", "dataNascimento", "dataCadastro")
+                .withIgnoreNullValues()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example<Autor> autorExample = Example.of(autor, matcher);
+        return autorRepository.findAll(autorExample);
+    }
+
+    public boolean possuiLivro(Autor autor){
+        return livroRepository.existsByAutor(autor);
     }
 }
