b74853ba9f3acc74b84e56285d10c4354bde8d60
Junior-Stranner <JuniorStranner@gmail.com>
2026-02-25 17:01:56 -0300

    made some improves in files and separeting some @beans to not has any dependencies with some methods

--- libraryapi/src/main/java/br/com/judev/libraryapi/config/SecurityConfiguration.java
+++ libraryapi/src/main/java/br/com/judev/libraryapi/config/SecurityConfiguration.java
@@ -10,9 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.core.GrantedAuthorityDefaults;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -39,11 +37,12 @@ public class SecurityConfiguration {
                 })
                 .build();
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
+}
+  /*  @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
     }
+}
 
     // CONFIGURA O PREFIXO ROLE
     @Bean
